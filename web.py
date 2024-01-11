38952abd23669fa5060e4516e707029273215114
innovativeclasses <40535538+innovativeclasses@users.noreply.github.com>
2024-01-11 13:25:57 +0530

    Update web.py

--- web.py
+++ web.py
@@ -72,19 +72,20 @@ if submit:
 		filtered=df[df["Zone/Circle/Division/Sub station"]==option]
 		#print(filtered.head(10))
 		#values_str = '\n'.join(filtered["col1"])
-		st.dataframe(filtered,hide_index=True)
-			
+		#st.dataframe(filtered,hide_index=True)
+		 st.session_state.filter_value = filtered
 	elif option2 and option2 == option2:
 		print ('dfdfdfd')
 		filtered=df[df["Zone/Circle/Division/Sub station"]==option2]
 		#values_str = '\n'.join(filtered["col1"])
 		#st.write(values_str)			
-		st.dataframe(filtered,hide_index=True)	
+		 st.session_state.filter_value = filtered
+	#st.dataframe(filtered,hide_index=True)	
 	elif option1 and option1 == option1:
 		print ('dffdsdsfdfdfd')
 		filtered=df[df["Zone/Circle/Division/Sub station"]==option1]
 		#values_str = '\n'.join(filtered["col1"])
 		#st.write(values_str)
-		st.dataframe(filtered,hide_index=True)
-
+		#st.dataframe(filtered,hide_index=True)
+		 st.session_state.filter_value = filtered
 
