32fda27bb7871f2e5fbb8555d627d5d39bada0a6
innovativeclasses <40535538+innovativeclasses@users.noreply.github.com>
2024-01-11 13:55:28 +0530

    Update Collection.py

--- pages/Collection.py
+++ pages/Collection.py
@@ -1,1 +1,27 @@
 
+
+
+import numpy as np	# np mean, np random
+import pandas as pd	 # read csv, df manipulation
+
+import streamlit as st	# 🎈 data web app development
+
+from streamlit_gsheets import GSheetsConnection
+
+
+
+st.set_page_config(
+	page_title="Real-Time Data Science Dashboard",
+	page_icon="chart_with_upwards_trend",
+	layout="centered",
+	
+)
+#data source
+
+
+st.title("MVVNL Dashboard Collection")
+
+filter_value = st.session_state.filtered
+st.dataframe(filter_value)
+
+#sidebar
